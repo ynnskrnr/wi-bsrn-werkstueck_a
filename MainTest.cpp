7e7c05587975cbdeffd410053ed8e982c16791a8
ynnskrnr <126953115+ynnskrnr@users.noreply.github.com>
2023-05-08 15:37:18 +0200

    Create MainTest.cpp
    Test Datei für die Alternative 2. Grobe Übersicht über die Funktionen

---
+++ MainTest.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "HalloWelt!" << endl;
+	return 0;
+}
+
+void createProcess() {
+
+}
+
+void forkProcess() {
+
+}
+
+void execProcess() {
+
+}
+
+void showProcessInformation() {
+
+}
+
+void visualize() {
+
+}
+
+void readFile() {
+
+}
+
+void writeFile() {
+
+}
+
+void collectGarbage() {
+
+}
