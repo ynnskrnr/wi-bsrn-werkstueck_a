fba35118bf766aa3ea72205b6851db55ce5f57e5
ynnskrnr <126953115+ynnskrnr@users.noreply.github.com>
2023-05-08 15:45:16 +0200

    Update MainTest.cpp
    Kommentare hinzugefügt

--- MainTest.cpp
+++ MainTest.cpp
@@ -8,26 +8,40 @@ int main()
 	return 0;
 }
 
+
+// Die Methode createProcess() erzeugt ein Prozess.
 void createProcess() {
 
 }
 
+// Die Methode forkProcess() erstellt ein Duplikat von dem erstellten Prozess aus createProcess().
 void forkProcess() {
 
 }
 
+// Die Methode execProcess() ersetzt den Prozess aus createProcess(). 
 void execProcess() {
 
 }
 
+// Die Methode showProcessInformation() gibt die zentralen Informationen der erzeugten Prozesse aus.
 void showProcessInformation() {
 
 }
 
+/*
+Die Methode visualize() erstellt textuell eine Baumstruktur in der Konsole
+und zeigt die Beziehung zwischen exec() und fork().
+*/ 
 void visualize() {
 
 }
 
+/*
+Die Methode writeFile() erzeugt eine Datei
+und speichert die zentralen Informationen der erzeugten Informationen aus der Methode showProcessInformation()
+in einer maschinenlesbaren Sprache.
+*/
 void readFile() {
 
 }
