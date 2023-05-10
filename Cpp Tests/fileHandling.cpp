e346919189468aa07b43bcc5dbf0bf408a5b71dc
Daniel Schor <daniel.schor@gmx.de>
2023-05-10 14:35:03 +0200

    Read/Write Fertig

--- Cpp Tests/fileHandling.cpp
+++ Cpp Tests/fileHandling.cpp
@@ -4,24 +4,45 @@
 
 using namespace std;
 
-int write(string path){
+// write text to file of given path
+int writeFile(string path, string text){
     ofstream f;
-    try
-    {       
-        f.open (path);
-        f << "Writing this to a file.\n";
+    try{       
+        f.open(path);
+        f << text;
         f.close();
         return 1;
     }
-    catch(const std::exception& e)
-    {
+    catch(exception e){
         return 0;
     }
 }
 
-int main()
-{
-    string path = "test.txt";
+// read text from file of given path
+string readFile(string path){
+    string line, s;
+    ifstream f(path);
 
+    if (f.is_open()){
+      while (getline(f,line)){
+        s += line + "\n";
+      }
+      f.close();
+      return s;
+    }
+    else return "Unable to open file"; 
+}
+
+int main(){
+
+    string  path = "test.txt",
+            text = "Hallo Welt!",
+            pathProc = "/proc/&pid/statm";
+
+    //writeFile(path, text);
+    //cout << readFile(path);
+    
+    cout << system("ps -eFw");
+    
     return 0;
 }
