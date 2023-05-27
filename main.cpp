47f32bbb049093e391b21feb6ee6e15add67ad39
Daniel Schor <daniel.schor@gmx.de>
2023-05-27 20:53:55 +0200

    Extraction to methods / new ui / cleaner main()

--- main.cpp
+++ main.cpp
@@ -44,6 +44,10 @@ void exec(const char *path)
     execl(path, args, (char *)NULL);
 }
 
+/*
+Vereinfachte Version der exec Familie
+Startet Programm mit angegebenen Pfad als String
+*/
 void exec(string path)
 {
     const char *pathC = path.c_str();
@@ -51,62 +55,83 @@ void exec(string path)
     execl(pathC, args, (char *)NULL);
 }
 
-void releaseResources(vector<pid_t>& prozesse) {
+void releaseResources(vector<pid_t> &prozesse)
+{
     int status;
-    for (pid_t pid : prozesse) {
-        if (waitpid(pid, &status, 0) == -1) {
+    for (pid_t pid : prozesse)
+    {
+        if (waitpid(pid, &status, 0) == -1)
+        {
             perror("waitpid fehlgeschlagen");
-        } else {
-            if (WIFEXITED(status)) {
+        }
+        else
+        {
+            if (WIFEXITED(status))
+            {
                 cout << "Kindprozess " << pid << " wurde beendet. Exit-Status: " << WEXITSTATUS(status) << endl;
-            } else if (WIFSIGNALED(status)) {
+            }
+            else if (WIFSIGNALED(status))
+            {
                 cout << "Kindprozess " << pid << " wurde durch Signal beendet: " << WTERMSIG(status) << endl;
             }
         }
     }
 }
 
+/*
+Clears the terminal and prints the menu
+*/
+void menu()
+{
+    system("clear");
+    string optionen[] = {
+        "Datum Ausgeben",
+        "PIDs Ausgeben",
+        "Beenden",
+        "Hello World! mit fork() und exec()",
+        "Clear"};
+    int optionenSize = sizeof(optionen) / sizeof(string);
+
+    for (int i = 0; i < optionenSize; i++)
+    {
+        cout << i + 1 << ". " << optionen[i] << endl;
+    }
+}
+
+int input()
+{
+    int option;
+
+    // Eingabe
+    cin >> option;
+    // Fehlereingabe abfangen
+    if (cin.fail())
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        option = 0;
+    }
+    
+    return option;
+}
+
 // main
 int main()
 {
-    string optionen[] = {"Datum Ausgeben",
-                         "PIDs Ausgeben",
-                         "Beenden",
-                         "Hello World! mit fork() und exec()"/*,
-                         "Freigabe test"*/};
     vector<pid_t> prozesse;
     // Fuegt Vaterprozess hinzu (sinnvoll, oder nicht? Zum bereinigen der ressourcen, nicht. Ansonsten schon.)
     // getpid() = syscall(SYS_getpid)
-    //prozesse.push_back(getpid());
+    // prozesse.push_back(getpid());
 
     bool running = true;
-    int option,
-        optionenSize = sizeof(optionen) / sizeof(string);
+    int option;
 
-    system("clear");
+    menu();
 
     while (running)
     {
-        // Menü
-        for (int i = 0; i < optionenSize; i++)
-        {
-            cout << i + 1 << ". " << optionen[i] << endl;
-        }
-
         // Eingabe
-        cin >> option;
-        // Fehlereingabe abfangen
-        if (cin.fail())
-        {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            option = 0;
-        }
-
-        // Sauberes Terminal
-        system("clear");
-        cout << endl
-             << "Gewaehlte Option: " << optionen[option - 1] << endl;
+        option = input();
 
         // Ausfuehrung der gewaehlten Option
         switch (option)
@@ -117,7 +142,6 @@ int main()
             {
                 date();
             }
-            usleep(100000);
             break;
         // PIDs ausgeben
         case 2:
@@ -139,13 +163,10 @@ int main()
             {
                 exec("./options/helloWorld");
             }
-            usleep(100000);
             break;
-        /*case 5:
-            system("ps");
-            releaseResources(prozesse);
-            system("ps");
-            break;*/
+        case 5:
+            menu();
+            break;
         // Falsche eingabe
         default:
             cout << "Falsche Eingabe" << endl;
