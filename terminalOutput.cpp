c4ed9235e84849285a9e6075870509048da6c036
Daniel Schor <118189502+Daniel-Schor@users.noreply.github.com>
2023-06-20 22:16:58 +0200

    Add files via upload

---
+++ terminalOutput.cpp
@@ -0,0 +1,8 @@
+1. Datum Ausgeben
+2. Prozessinformationen ausgeben
+3. "Hello World!" ausgeben
+4. Prozesshyrachie anzeigen
+5. Logdatei lesen
+6. Konsole bereinigen
+7. Beenden und Systemressourcen freigeben
+
